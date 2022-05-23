43aa76440707b152d29298fb9c48d66f56dcd3c6
dishant-baraily <97446132+dishant-baraily@users.noreply.github.com>
2022-05-23 17:04:35 +0530

    e1

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "../Counter/Counter";
 import styles from "./addTask.module.css";
 
 const AddTask = () => {
   // NOTE: do not delete `data-cy` key value pair
+  const [task, setTask] = useState("");
+  const [add, setAdd] = useState([]);
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  }
+  const handleClick = () => {
+    setAdd([...add,task]);
+  }
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
+      <div className={styles.heading}>
+          <h1>Todo List</h1> <h3>You have 3 of 5 tasks remaining</h3>
+      </div>
+      <input data-cy="add-task-input" type="text" placeholder="     Add task..." onChange={handleChange} />
+      <button data-cy="add-task-button" onClick={handleClick}>+</button>
+      <hr />
+      {add.map((item) => {
+        return <div>{item}</div>
+      })}
     </div>
   );
 };
--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 import styles from "./taskApp.module.css";
+import AddTask from "./AddTask/AddTask"
+import Tasks from "./Tasks/Tasks";
+import TaskHeader from "./TaskHeader/TaskHeader";
 
 const TaskApp = () => {
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div data-cy="task-app" className={styles.taskApp}>
-      {/* Header */}
-      {/* Add Task */}
-      {/* Tasks */}
+      {/*<TaskHeader />*/}
+      <AddTask />
+      <Tasks />
     </div>
   );
 };
